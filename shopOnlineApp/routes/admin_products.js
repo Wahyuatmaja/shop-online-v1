b21d85537b87623157ab97e835a7ba215c4c492b
vanbumi <jsp.dio@gmaile.com>
2018-10-26 21:50:02 +0700

    Materi 25 branch fastdev25 Post Edit Product done perfectly

--- shopOnlineApp/routes/admin_products.js
+++ shopOnlineApp/routes/admin_products.js
@@ -171,51 +171,72 @@ router.get("/edit-product/:id", function(req, res) {
   });
 });
 
-// Membuat method POST pada edit page
-router.post("/edit-page/:slug", function(req, res) {
-  req.checkBody("title", "Title must have a value").notEmpty();
-  req.checkBody("content", "Content must have a value").notEmpty();
+// Membuat method POST pada edit product
+router.post("/edit-product/:id", function(req, res) {
+  var imageFile =
+    typeof req.files.image !== "undefined" ? req.files.image.name : "";
+
+  req.checkBody("title", "Title must have a value.").notEmpty();
+  req.checkBody("desc", "Description must have a value.").notEmpty();
+  req.checkBody("price", "Price must have a value.").isDecimal();
+  req.checkBody("image", "You must upload an image").isImage(imageFile);
 
   var title = req.body.title;
-  var slug = req.body.slug.replace(/\s+/g, "-").toLowerCase();
-  if (slug == "") slug = title.replace(/\s+/g, "-").toLowerCase();
-
-  var content = req.body.content;
-  var id = req.body.id;
+  var slug = title.replace(/\s+/g, "-").toLowerCase();
+  var desc = req.body.desc;
+  var price = req.body.price;
+  var category = req.body.category;
+  var pimage = req.body.pimage;
+  var id = req.params.id;
 
   var errors = req.validationErrors();
 
   if (errors) {
-    res.render("admin/edit_page", {
-      errors: errors,
-      title: title,
-      slug: slug,
-      content: content,
-      id: id
-    });
+    req.session.errors = errors;
+    res.redirect("/admin/products/edit-product/" + id);
   } else {
-    Page.findOne({ slug: slug, _id: { $ne: id } }, function(err, page) {
-      if (page) {
-        req.flash("danger", "Page slug is exist, choose another slug");
-        res.render("admin/edit_page", {
-          title: title,
-          slug: slug,
-          content: content,
-          id: id
-        });
+    Product.findOne({ slug: slug, _id: { $ne: id } }, function(err, p) {
+      if (err) console.log(err);
+
+      if (p) {
+        req.flash("danger", "Product title exists, choose another.");
+        res.redirect("/admin/products/edit-product/" + id);
       } else {
-        Page.findById(id, function(err, page) {
-          if (err) return console.log(err);
+        Product.findById(id, function(err, p) {
+          if (err) console.log(err);
 
-          page.title = title;
-          page.slug = slug;
-          page.content = content;
+          p.title = title;
+          p.slug = slug;
+          p.desc = desc;
+          p.price = parseFloat(price).toFixed(2);
+          p.category = category;
+          if (imageFile != "") {
+            p.image = imageFile;
+          }
 
-          page.save(function(err) {
-            if (err) return console.log(err);
+          p.save(function(err) {
+            if (err) console.log(err);
 
-            req.flash("success", "Page has been update!");
-            res.redirect("/admin/pages/edit-page/" + page.slug);
+            if (imageFile != "") {
+              if (pimage != "") {
+                fs.remove(
+                  "public/product_images/" + id + "/" + pimage,
+                  function(err) {
+                    if (err) console.log(err);
+                  }
+                );
+              }
+
+              var productImage = req.files.image;
+              var path = "public/product_images/" + id + "/" + imageFile;
+
+              productImage.mv(path, function(err) {
+                return console.log(err);
+              });
+            }
+
+            req.flash("success", "Product edited!");
+            res.redirect("/admin/products/edit-product/" + id);
           });
         });
       }
