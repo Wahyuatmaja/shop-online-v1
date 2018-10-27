87cc3e7881c75ab13af6dda5ade034b85a94efc3
vanbumi <jsp.dio@gmaile.com>
2018-10-27 21:48:23 +0700

    Materi 30 branch fastdev30 Admin Page Change  done perfectly

--- shopOnlineApp/routes/admin_pages.js
+++ shopOnlineApp/routes/admin_pages.js
@@ -1,27 +1,28 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 // Get Category model.
-var Page = require('../models/page');
+var Page = require("../models/page");
 
 // Get pages index
-router.get('/', function(req, res){
+router.get("/", function(req, res) {
   // mengambil data dari database
-  Page.find({}).sort({sorting: 1}).exec(function(err, pages) {
-    res.render('admin/pages', {
-      pages: pages
+  Page.find({})
+    .sort({ sorting: 1 })
+    .exec(function(err, pages) {
+      res.render("admin/pages", {
+        pages: pages
+      });
     });
-  });
 });
 
 // Membuat add page dengan method GET
-router.get('/add-page', function(req, res) {
-
+router.get("/add-page", function(req, res) {
   var title = "";
   var slug = "";
   var content = "";
 
-  res.render('admin/add_page', {
+  res.render("admin/add_page", {
     title: title,
     slug: slug,
     content: content
@@ -29,31 +30,30 @@ router.get('/add-page', function(req, res) {
 });
 
 // Membuat method POST pada add page
-router.post('/add-page', function(req, res) {
-
-  req.checkBody('title', 'Title must have a value').notEmpty();
-  req.checkBody('content', 'Content must have a value').notEmpty();
+router.post("/add-page", function(req, res) {
+  req.checkBody("title", "Title must have a value").notEmpty();
+  req.checkBody("content", "Content must have a value").notEmpty();
 
   var title = req.body.title;
-  var slug = req.body.slug.replace(/\s+/g, '-').toLowerCase();
-  if (slug == '') slug = title.replace(/\s+/g, '-').toLowerCase();
-  
+  var slug = req.body.slug.replace(/\s+/g, "-").toLowerCase();
+  if (slug == "") slug = title.replace(/\s+/g, "-").toLowerCase();
+
   var content = req.body.content;
 
   var errors = req.validationErrors();
 
   if (errors) {
-    res.render('admin/add_page', {
+    res.render("admin/add_page", {
       errors: errors,
       title: title,
       slug: slug,
       content: content
     });
   } else {
-    Page.findOne({slug: slug}, function(err, page) {
+    Page.findOne({ slug: slug }, function(err, page) {
       if (page) {
-        req.flash('danger', 'Page slug is exist, choose another slug');
-        res.render('admin/add_page', {
+        req.flash("danger", "Page slug is exist, choose another slug");
+        res.render("admin/add_page", {
           title: title,
           slug: slug,
           content: content
@@ -66,26 +66,30 @@ router.post('/add-page', function(req, res) {
           sorting: 100
         });
 
-        page.save(function (err) {
-          if (err)
-            return console.log(err);
+        page.save(function(err) {
+          if (err) return console.log(err);
 
-          req.flash('success', 'Page added!');
-          res.redirect('/admin/pages')
+          // add here
+          Page.find({})
+            .sort({ sorting: 1 })
+            .exec(function(err, pages) {
+              if (err) {
+                console.log(err);
+              } else {
+                req.app.locals.pages = pages;
+              }
+            });
+
+          req.flash("success", "Page added!");
+          res.redirect("/admin/pages");
         });
       }
     });
   }
 });
 
-// Membuat POST sortable pages
-
-router.post('/reorder-pages', function(req, res) {
-
-  // console.log(req.body);
-
-  var ids = req.body['id[]'];
-
+// Sort pages function
+function sortPages(ids, callback) {
   var count = 0;
 
   for (var i = 0; i < ids.length; i++) {
@@ -93,51 +97,69 @@ router.post('/reorder-pages', function(req, res) {
     count++;
 
     (function(count) {
-      Page.findById(id, function (err, page) {
+      Page.findById(id, function(err, page) {
         page.sorting = count;
-        page.save(function (err) {
-          if (err)
-            return console.log(err)
+        page.save(function(err) {
+          if (err) return console.log(err);
+          ++count;
+          if (count >= ids.length) {
+            callback();
+          }
         });
       });
     })(count);
   }
+}
+
+// Membuat POST sortable pages
+router.post("/reorder-pages", function(req, res) {
+  // console.log(req.body);
+
+  var ids = req.body["id[]"];
+
+  sortPages(ids, function() {
+    Page.find({})
+      .sort({ sorting: 1 })
+      .exec(function(err, pages) {
+        if (err) {
+          console.log(err);
+        } else {
+          req.app.locals.pages = pages;
+        }
+      });
+  });
 });
 
 // GET edit page
+router.get("/edit-page/:slug", function(req, res) {
+  Page.findOne({ slug: req.params.slug }, function(err, page) {
+    if (err) return console.log(err);
 
-router.get('/edit-page/:slug', function(req, res) {
-
-  Page.findOne({slug: req.params.slug}, function(err, page) {
-    if (err)
-      return console.log(err);
-
-    res.render('admin/edit_page', {
+    res.render("admin/edit_page", {
       title: page.title,
       slug: page.slug,
       content: page.content,
       id: page._id
-    });  
+    });
   });
 });
 
 // Membuat method POST pada edit page
-router.post('/edit-page/:slug', function(req, res) {
-
-  req.checkBody('title', 'Title must have a value').notEmpty();
-  req.checkBody('content', 'Content must have a value').notEmpty();
+router.post("/edit-page/:slug", function(req, res) {
+  req.checkBody("title", "Title must have a value").notEmpty();
+  req.checkBody("content", "Content must have a value").notEmpty();
 
   var title = req.body.title;
-  var slug = req.body.slug.replace(/\s+/g, '-').toLowerCase();
-  if (slug == '') slug = title.replace(/\s+/g, '-').toLowerCase();
-  
+  var slug = req.body.slug.replace(/\s+/g, "-").toLowerCase();
+  if (slug == "") slug = title.replace(/\s+/g, "-").toLowerCase();
+
   var content = req.body.content;
   var id = req.body.id;
 
   var errors = req.validationErrors();
 
   if (errors) {
-    res.render('admin/edit_page', {
+    res.render("admin/edit_page", {
       errors: errors,
       title: title,
       slug: slug,
@@ -145,51 +167,66 @@ router.post('/edit-page/:slug', function(req, res) {
       id: id
     });
   } else {
-    Page.findOne({slug: slug, _id:{'$ne':id}}, function(err, page) {
-      
+    Page.findOne({ slug: slug, _id: { $ne: id } }, function(err, page) {
       if (page) {
-        req.flash('danger', 'Page slug is exist, choose another slug');
-        res.render('admin/edit_page', {
+        req.flash("danger", "Page slug is exist, choose another slug");
+        res.render("admin/edit_page", {
           title: title,
           slug: slug,
           content: content,
           id: id
         });
       } else {
-        Page.findById(id, function (err, page) {
-          if (err) 
-            return console.log(err);
+        Page.findById(id, function(err, page) {
+          if (err) return console.log(err);
 
-            page.title = title;
-            page.slug = slug;
-            page.content = content;
+          page.title = title;
+          page.slug = slug;
+          page.content = content;
 
-            page.save(function (err) {
-              if (err)
-                return console.log(err);
-    
-              req.flash('success', 'Page has been update!');
-              res.redirect('/admin/pages/edit-page/'+page.slug);
-            });
+          page.save(function(err) {
+            if (err) return console.log(err);
 
+            // add here
+            Page.find({})
+              .sort({ sorting: 1 })
+              .exec(function(err, pages) {
+                if (err) {
+                  console.log(err);
+                } else {
+                  req.app.locals.pages = pages;
+                }
+              });
+
+            req.flash("success", "Page has been update!");
+            res.redirect("/admin/pages/edit-page/" + page.slug);
+          });
         });
-
       }
     });
   }
 });
 
 // GET delete page
-router.get('/delete-pages/:id', function (req, res) {
-  Page.findByIdAndRemove(req.params.id, function (err) {
-      if (err)
-          return console.log(err);
+router.get("/delete-pages/:id", function(req, res) {
+  Page.findByIdAndRemove(req.params.id, function(err) {
+    if (err) return console.log(err);
 
-      req.flash('success', 'Page deleted!');
-      res.redirect('/admin/pages/');
+    // add here
+    Page.find({})
+      .sort({ sorting: 1 })
+      .exec(function(err, pages) {
+        if (err) {
+          console.log(err);
+        } else {
+          req.app.locals.pages = pages;
+        }
+      });
+
+    req.flash("success", "Page deleted!");
+    res.redirect("/admin/pages/");
   });
 });
 
 // export
 module.exports = router;
-
