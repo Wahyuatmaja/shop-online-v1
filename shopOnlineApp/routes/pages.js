4e96af9997189f92b65a7373dfa254eb64c8b234
vanbumi <jsp.dio@gmaile.com>
2018-10-28 20:23:58 +0700

    Materi 31 branch fastdev31 Display Page Content

--- shopOnlineApp/routes/pages.js
+++ shopOnlineApp/routes/pages.js
@@ -1,12 +1,38 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+
+// Get Page model
+var Page = require("../models/page");
 
 // export
 module.exports = router;
 
-// Home / index
-router.get('/', function(req, res){
-  res.render("index", {
-    title : 'Home'
-  })
+// Get Home / index
+router.get("/", function(req, res) {
+  Page.findOne({ slug: "home" }, function(err, page) {
+    if (err) console.log(err);
+
+    res.render("index", {
+      title: page.title,
+      content: page.content
+    });
+  });
+});
+
+// Get a page
+router.get("/:slug", function(req, res) {
+  var slug = req.params.slug;
+
+  Page.findOne({ slug: slug }, function(err, page) {
+    if (err) console.log(err);
+
+    if (!page) {
+      res.redirect("/");
+    } else {
+      res.render("index", {
+        title: page.title,
+        content: page.content
+      });
+    }
+  });
 });
